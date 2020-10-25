f8491b0c224adc0182ea23a76d8c1db7d23e6cb4
joungeun <lje0818ovo@naver.com>
2020-10-25 22:36:24 +0900

    html 추가, 회원가입 구현 error 1
    main, login, signUp html을 추가했다. from 태그 사용법을 익혀야 한다. 회원가입 하는 UserInsert 컨트롤러 메소드를 만들었고 JoinInsert Dao를 만들었다. UserInsert 생성자에서 오류가 났다.

--- SpringBoot/board/src/main/java/com/dsm/board/controller/UserController.java
+++ SpringBoot/board/src/main/java/com/dsm/board/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.dsm.board.controller;
 import com.dsm.board.dao.UserDao;
 import com.dsm.board.dto.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -13,8 +16,22 @@ public class UserController {
 
     private UserDao ud;
 
-    public UserController(){
-        List<UserDto> userLiset = ud.loginSelect();
+    @Autowired
+    public UserController(UserDao ud){ this.ud = ud;}
+
+
+
+    // 회원가입
+    @GetMapping(value = "info/{id}/{pw}/{name}/{age}/{introduce}")
+    public String UserInsert(
+            @PathVariable("id") String id,
+            @PathVariable("pw") String pw,
+            @PathVariable("name") String name,
+            @PathVariable("age") int age, @PathVariable("introduce") String introduce){
+
+        ud.JoinInsert(id, pw, name, age, introduce);
+
+        return "login"; // 로그인 페이지 리턴
     }
 
     // 우선 디비 만들기
--- SpringBoot/board/src/main/java/com/dsm/board/dao/UserDao.java
+++ SpringBoot/board/src/main/java/com/dsm/board/dao/UserDao.java
@@ -1,9 +1,7 @@
 package com.dsm.board.dao;
 import com.dsm.board.dto.UserDto;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,10 +17,44 @@ public class UserDao {
             e.printStackTrace();
         }
     }
-    // forname은 문자열로부터 class 객체를 얻는다.
+    // 드라이버 load
 
-    public List<UserDto> loginSelect(){
+    public List<UserDto> JoinInsert(String id, String pw, String name, int age, String introduce){
         List<UserDto> userList = new ArrayList<>();
+        String url = "jdbc:mysql://localhost/board?serverTimezone=UTC";
+        String user = "root";
+        String password="0818";
+        String sql = "INSERT INTO user (id, pw, name, age, introduce) VALUES (?,?,?,?,?)";
+
+        try{
+            conn = DriverManager.getConnection(url, user, password);
+            // db에 접속할 수 있도록 해준다. 접속에 필요한 정보인 db url, username, pw를 보내야 한다
+            pstmt = conn.prepareStatement(sql);
+            // Connection 객체의 pre~ 메소드의 매개변수로 sql 문을 보내준다
+            pstmt.setString(1, id);
+            pstmt.setString(2, pw);
+            pstmt.setString(3, name);
+            pstmt.setInt(4,age);
+            pstmt.setString(5, introduce);
+            pstmt.executeUpdate();
+        } catch(SQLException e){
+            e.printStackTrace();
+        } finally{
+            try{
+                if(conn!=null){
+                    conn.close();
+                }
+                if(pstmt!=null){
+                    pstmt.close();
+                }
+                if(rs!=null){
+                    rs.close();
+                }
+            } catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
         return userList;
     }
 }
