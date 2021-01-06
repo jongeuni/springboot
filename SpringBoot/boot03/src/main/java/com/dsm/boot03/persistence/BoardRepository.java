f4b0c3012fbe9ed88c70bc49c9437956b4f4882a
joungeun <lje0818ovo@naver.com>
2021-01-07 02:17:09 +0900

    Add testByWriter, testByWriterContaining

--- SpringBoot/boot03/src/main/java/com/dsm/boot03/persistence/BoardRepository.java
+++ SpringBoot/boot03/src/main/java/com/dsm/boot03/persistence/BoardRepository.java
@@ -3,10 +3,15 @@ package com.dsm.boot03.persistence;
 import com.dsm.boot03.domain.Board;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface BoardRepository extends CrudRepository<Board, Long> {
 
     public List<Board> findBoardByTitle(String title);
 
+    public Collection<Board> findByWriter(String writer);
+
+    // 작성자 이름에 ~가 들어가있는 게시글을 검색하려고 함
+    public Collection<Board> findByWriterContaining(String writer);
 }
--- SpringBoot/boot03/src/test/java/com/dsm/boot03/Boot03ApplicationTests.java
+++ SpringBoot/boot03/src/test/java/com/dsm/boot03/Boot03ApplicationTests.java
@@ -6,14 +6,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Collection;
+
 @SpringBootTest //ExtendWith(SpringE)
 class Boot03ApplicationTests {
 
 	@Autowired
 	private BoardRepository repo;
 
-
-
+	// insert 200 개
 	/*@Test
 	public void testInsert200(){
 		for(int i=1; i<=200; i++){
@@ -25,11 +26,31 @@ class Boot03ApplicationTests {
 		}
 	}*/
 
-	@Test
+	// title로 select
+	/*@Test
 	public void testByTitle(){
 		System.out.println("찾기전");
 		repo.findBoardByTitle("제목..177").forEach(board-> System.out.println(board)); // return 해 준 Board Entity 리스트를 하나씩 board에 담는 건가???
 		System.out.println("찾음");
+	}*/
+
+	// writer로 select
+	/*@Test
+	public void testByWriter(){
+		Collection<Board> results = repo.findByWriter("user00");
+
+		results.forEach(
+				board -> System.out.println(board)
+		);
+	}*/
+
+	@Test
+	public void testByWriterContaining(){
+		// 작성자 이름에 05가 들어있는 게시글 검색
+		// % 키워드 %
+		Collection<Board> results = repo.findByWriterContaining("05");
+
+		results.forEach(board -> System.out.println(board));
 	}
 
 	@Test
