b15bd75fd31d538785213437e23663cb003d0319
joungeun <lje0818ovo@naver.com>
2021-02-06 22:29:45 +0900

    @Modifying

--- SpringBoot/boot04/src/main/java/com/dsm/boot04/persistence/PDSBoardRepository.java
+++ SpringBoot/boot04/src/main/java/com/dsm/boot04/persistence/PDSBoardRepository.java
@@ -1,8 +1,16 @@
 package com.dsm.boot04.persistence;
 
 import com.dsm.boot04.domain.PDSBoard;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import javax.transaction.Transactional;
 
 public interface PDSBoardRepository extends CrudRepository<PDSBoard, Long> {
 
+
+    @Modifying // @Query가 DML 작업을 처리할 수 있도록 (INSERT, UPDATE, DELETE)
+    @Query("update PDSFile f set f.pdsfile = ?2 where f.fno = ?1")
+    public void updatePDSFile(Long fno, String enwFileName);
 }
--- SpringBoot/boot04/src/test/java/com/dsm/boot04/PDSBoardTests.java
+++ SpringBoot/boot04/src/test/java/com/dsm/boot04/PDSBoardTests.java
@@ -6,14 +6,17 @@ import com.dsm.boot04.persistence.PDSBoardRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 
+import javax.transaction.Transactional;
 import java.util.Arrays;
 
 @SpringBootTest
+@Commit
 public class PDSBoardTests {
     @Autowired
     PDSBoardRepository repo;
-    @Test
+    /*@Test
     public void testInsertPDS(){
         PDSBoard pds = new PDSBoard();
         pds.setPname("Document");
@@ -27,5 +30,13 @@ public class PDSBoardTests {
         pds.setFiles(Arrays.asList(file1,file2));
 
         repo.save(pds);
+    }*/
+
+    @Transactional // delete 나 update 사용하는 경우 트랜잭션 처리를 해줘야 함
+    @Test
+    public void testUpdateFileName1(){
+        Long fno = 1L;
+        String newName = "updateFile1.doc";
+        repo.updatePDSFile(fno, newName);
     }
 }
