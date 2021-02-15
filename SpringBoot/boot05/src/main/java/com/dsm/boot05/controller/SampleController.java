92f1b9fae0266648e23b3751617da008e8ad718f
joungeun <lje0818ovo@naver.com>
2021-02-15 22:20:25 +0900

    Thymeleaf 동작 확인

---
+++ SpringBoot/boot05/src/main/java/com/dsm/boot05/controller/SampleController.java
@@ -0,0 +1,14 @@
+package com.dsm.boot05.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SampleController {
+
+    @GetMapping("/sample1")
+    public void sample1(Model model){
+        model.addAttribute("greeting","Hello world");
+    }
+}
