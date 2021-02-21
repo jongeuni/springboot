884286342f6c0ecfc2e774d20cc047a946f5d58f
joungeun <lje0818ovo@naver.com>
2021-02-21 23:51:31 +0900

    페이징 테스트

--- SpringBoot/boot06re/src/main/java/com/dsm/boot06re/persistence/WebBoardRepository.java
+++ SpringBoot/boot06re/src/main/java/com/dsm/boot06re/persistence/WebBoardRepository.java
@@ -1,8 +1,23 @@
 package com.dsm.boot06re.persistence;
 
+import com.dsm.boot06re.domain.QWebBoard;
 import com.dsm.boot06re.domain.WebBoard;
+import com.querydsl.core.BooleanBuilder;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
+import com.querydsl.core.types.Predicate;
 
 public interface WebBoardRepository extends CrudRepository<WebBoard, Long>, QuerydslPredicateExecutor<WebBoard> {
+    public default Predicate makePredicate(String type, String keyword){
+        BooleanBuilder builder = new BooleanBuilder();
+
+        QWebBoard board = QWebBoard.webBoard;
+
+        // type if~else
+
+        //bno>0
+        builder.and(board.bno.gt(0));
+
+        return  builder;
+    }
 }
--- SpringBoot/boot06re/src/test/java/com/dsm/boot06re/WebBoardRepositoryTests.java
+++ SpringBoot/boot06re/src/test/java/com/dsm/boot06re/WebBoardRepositoryTests.java
@@ -2,20 +2,28 @@ package com.dsm.boot06re;
 
 import com.dsm.boot06re.domain.WebBoard;
 import com.dsm.boot06re.persistence.WebBoardRepository;
+import lombok.extern.java.Log;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Commit;
 
 import java.util.stream.IntStream;
 
+import static org.reflections.Reflections.log;
+
+@Log
 @SpringBootTest
 @Commit
 public class WebBoardRepositoryTests {
     @Autowired
     WebBoardRepository repo;
 
-    @Test // 더미 데이터 추가
+    /*@Test // 더미 데이터 추가
     public void insertBoardDummies(){
         IntStream.range(0,300).forEach(i->{
             WebBoard board = new WebBoard();
@@ -26,5 +34,17 @@ public class WebBoardRepositoryTests {
 
             repo.save(board);
         });
+    }*/
+
+    @Test
+    public void testList1(){
+        Pageable pageable = PageRequest.of(0,20, Sort.Direction.DESC, "bno");
+
+        Page<WebBoard> result = repo.findAll(repo.makePredicate(null,null),pageable);
+
+        log.info("PAGE: "+result.getPageable());
+
+        log.info("---------------");
+        result.getContent().forEach(board->log.info(""+board));
     }
 }
