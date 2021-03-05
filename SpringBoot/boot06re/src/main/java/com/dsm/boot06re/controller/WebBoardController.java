799b776afa884868e4bcbe5a812ab325d34378c1
joungeun <lje0818ovo@naver.com>
2021-03-05 20:06:08 +0900

    <table> and page processing

--- SpringBoot/boot06re/src/main/java/com/dsm/boot06re/controller/WebBoardController.java
+++ SpringBoot/boot06re/src/main/java/com/dsm/boot06re/controller/WebBoardController.java
@@ -13,6 +13,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -24,10 +25,10 @@ public class WebBoardController {
     private WebBoardRepository repo;
 
     @GetMapping("/list")
-    public void list(PageVO vo, Model model){
+    public void list(@ModelAttribute ("PageVO") PageVO vo, Model model){
         Pageable page = vo.makePageable(0,"bno");
 
-        Page<WebBoard> result = repo.findAll(repo.makePredicate(null,null),page);
+        Page<WebBoard> result = repo.findAll(repo.makePredicate(vo.getType(),vo.getKeyword()),page);
 
         log.info(""+page);
         log.info(""+result);
