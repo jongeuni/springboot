a78262d0ea604344cfd19906e999ba97d17004de
joungeun <lje0818ovo@naver.com>
2021-03-07 19:31:22 +0900

    Register Posts

--- SpringBoot/boot06re/src/main/java/com/dsm/boot06re/controller/WebBoardController.java
+++ SpringBoot/boot06re/src/main/java/com/dsm/boot06re/controller/WebBoardController.java
@@ -14,7 +14,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/boards/")
@@ -38,5 +40,20 @@ public class WebBoardController {
         model.addAttribute("result",new PageMaker(result));
     }
 
+    @GetMapping("/register")
+    public void registerGET(@ModelAttribute("vo")WebBoard vo){
+        log.info("register get");
+    }
+
+    @PostMapping("/register")
+    public String registerPOST(@ModelAttribute("vo")WebBoard vo, RedirectAttributes rttr){
+        log.info("register post");
+        log.info(""+vo);
+
+        repo.save(vo);
+        rttr.addFlashAttribute("msg","success");
+
+        return "redirect:/boards/list";
+    }
 
 }
