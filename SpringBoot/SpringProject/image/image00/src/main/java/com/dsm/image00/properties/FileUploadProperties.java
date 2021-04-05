fb4ab459a67ac447e49b60039cdb50f883ff5571
joungeun <lje0818ovo@naver.com>
2021-04-05 19:36:19 +0900

    [ADD] FileUploadProperties, FileController, FileService

---
+++ SpringBoot/SpringProject/image/image00/src/main/java/com/dsm/image00/properties/FileUploadProperties.java
@@ -0,0 +1,17 @@
+package com.dsm.image00.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+@ConfigurationProperties(prefix = "file.upload")
+public class FileUploadProperties {
+    private String location;
+    public String getLocation(){
+        return location;
+    }
+
+    public void setLocation(String location){
+        this.location = location;
+    }
+}
