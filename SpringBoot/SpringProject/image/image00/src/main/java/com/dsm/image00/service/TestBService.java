885b693815bf5db988688d213701c8679e1305be
joungeun <lje0818ovo@naver.com>
2021-04-06 22:42:12 +0900

    [FIX&REMOVE] Fix File Generation Errors & Delete Annotation

--- SpringBoot/SpringProject/image/image00/src/main/java/com/dsm/image00/service/TestBService.java
+++ SpringBoot/SpringProject/image/image00/src/main/java/com/dsm/image00/service/TestBService.java
@@ -4,6 +4,8 @@ import com.dsm.image00.domain.FilesEntity;
 import com.dsm.image00.repository.FilesRepository;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,7 +22,6 @@ public class TestBService {
     // 업로드 경로
     private final String uploadPath = Paths.get("D:", "imagePr").toString();
 
-
     // 랜덤 문자열
 
     private final String getRandomString(){
@@ -28,54 +29,35 @@ public class TestBService {
     }
 
     // 업로드
-    public String uploadFiles(MultipartFile file){
+    public ResponseEntity uploadFiles(MultipartFile file){
         if(file == null){
-            return "파일 비어있음";
-            //FilesEntity filesEntity = new FilesEntity();
-            //return filesEntity; // 파일 비어있을 경우
+            return new ResponseEntity("파일이 비어있음.",HttpStatus.BAD_REQUEST); // 파일 비어있을 경우
         }
-        File dir = new File(uploadPath+"\\"+file.getOriginalFilename());
-        System.out.println(dir+"  dir");
-        if(dir.exists()){ // 파일 존재 여부를 알 수 있다
-            System.out.println("이미 존재하는 파일");
 
-        } else{
+        File dir = new File(uploadPath);
+        if(dir.exists()!=false){ // 파일 존재 여부를 알 수 있다
             dir.mkdir(); // 파일 존재 안 하면 만들기
         }
 
         try{
 
-            String extension = FilenameUtils.getExtension(file.getOriginalFilename()); // 확장자
+            //String extension = FilenameUtils.getExtension(file.getOriginalFilename()); // 확장자
 
+            String saveName = getRandomString()+file.getOriginalFilename(); // 서버에 저장할 파일명, 중복되면 안 됨
 
-            String saveName = getRandomString()+file.getOriginalFilename();
-            System.out.println(saveName);
-            // 서버에 저장할 파일명, 중복되면 안됨.
-
-            File target = new File(uploadPath, saveName);
-            file.transferTo(target);
-            // 업로드 경로에 saveName과 동일한 이름을 가진 파일 생성
-            System.out.println(target+"     target");
+            File target = new File(uploadPath, saveName); // 업로드 경로와 파일명 담긴 파일 객체 생성
+            file.transferTo(target);  // upload 경로에 saveName과 동일한 이름 가진 파일 생성
 
             FilesEntity filesEntity = new FilesEntity();
-            filesEntity.setFilename(saveName); // 서버에 저장할 이름
+            filesEntity.setFilename(saveName);
             filesEntity.setFileOriName(file.getOriginalFilename());
             filesEntity.setFileurl(target.toString());
 
-            System.out.println(filesEntity+"  파일엔티티");
-            System.out.println(saveName+"   세이브네임");
             filesRepository.save(filesEntity);
-
         } catch(IOException e){
-            System.out.println("["+file.getOriginalFilename()+"] failed to ave file...");
+            return new ResponseEntity("파일이 발견되지 않음. 저장 안 됨.",HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 
-
-        } /*catch(Exception e){
-            System.out.println("["+file.getOriginalFilename()+"] failed to save file...");
-
-
-        }*/
-
-        return "완료?";
+        return new ResponseEntity("파일 업로드 완료.",HttpStatus.OK);
     }
 }
